145a4a644e4d24caf5eb07d8a272dbf773818309
DoDo0002 <118946887+DoDo0002@users.noreply.github.com>
2023-01-15 14:30:02 +0800

    Nice to meet you:>

---
+++ resume.js
@@ -0,0 +1,8 @@
+const loading = document.querySelector(".loading-page-container");
+
+console.log(loading);
+
+// 網頁載入時假載入頁延時關閉
+window.addEventListener("load", function () {
+  setTimeout('loading.style.display = "none"', 4000);
+});
